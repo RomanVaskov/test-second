ae13cd139c606adcfc159fc92030daa02f6b7be2
Roman <romavaskov@yandex.ru>
2020-02-14 18:41:22 +0300

    Реализовал динамическую работу API, а также вывод ошибки

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./spinner.css";
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-spin-rtu2rs5vs0b">
+            <div className="ldio-65lyfspn57t">
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Spinner;
