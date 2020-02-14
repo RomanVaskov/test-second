336118e2b5f1678f977e4e6c19fe042bee9573b7
Roman <romavaskov@yandex.ru>
2020-02-14 13:02:27 +0300

    Updates

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import { Col, Row, Container } from 'reactstrap';
+import {Col, Row, Container} from 'reactstrap';
 import Header from '../header';
 import RandomChar from '../randomChar';
 import ItemList from '../itemList';
 import CharDetails from '../charDetails';
 
+
 const App = () => {
     return (
-        <>
+        <> 
             <Container>
                 <Header />
             </Container>
             <Container>
                 <Row>
-                    <Col lg={{ size: 5, offset: 0 }}>
-                        <RandomChar />
+                    <Col lg={{size: 5, offset: 0}}>
+                        <RandomChar/>
                     </Col>
                 </Row>
                 <Row>
